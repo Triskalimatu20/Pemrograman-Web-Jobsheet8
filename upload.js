e9e6bea9e2a8a4e379419ae67f24ffbab593d189
Triskalimatu20 <triskalimatu@gmail.com>
2025-10-22 08:15:02 +0700

    3_upload ajax

---
+++ upload.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $("#upload-form").submit(function (e) {
+    e.preventDefault(); // Mencegah reload halaman
+
+    var formData = new FormData(this); // Ambil data dari form
+
+    $.ajax({
+      type: "POST",
+      url: "upload_ajax.php", // File PHP tujuan
+      data: formData,
+      cache: false, // HARUS tanpa tanda kutip
+      contentType: false,
+      processData: false,
+      success: function (response) {
+        $("#status").html(response); // Tampilkan hasil dari PHP
+      },
+      error: function () {
+        $("#status").html("Terjadi kesalahan saat mengunggah file.");
+      },
+    });
+  });
+});
