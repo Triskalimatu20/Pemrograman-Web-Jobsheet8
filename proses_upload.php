000b82d73da5e8dcb582e6a123bfbb74f32c7bed
Triskalimatu20 <triskalimatu@gmail.com>
2025-10-22 07:07:28 +0700

    2_modif multiupload

--- proses_upload.php
+++ proses_upload.php
@@ -1,27 +1,36 @@
 <?php
-// Lokasi penyimpanan file yang diunggah
-$targetDirectory = "documents/";
+// Lokasi penyimpanan file gambar
+$targetDirectory = "uploads/";
 
-// Periksa apakah direktori penimpanan ada, jika tidak maka buat
-if(!file_exists($targetDirectory)) {
+// Membuat folder uploads jika belum ada
+if (!file_exists($targetDirectory)) {
     mkdir($targetDirectory, 0777, true);
 }
 
-if($_FILES['files']['name'][0]) {
-    $totalFiles = count($_FILES['files']['name']);
+// Periksa apakah ada file yang diunggah
+if (!empty($_FILES['images']['name'][0])) {
+    $totalFiles = count($_FILES['images']['name']);
+    $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
 
-    // Loop melalui semua file yang diunggah
+    // Loop untuk setiap file gambar
     for ($i = 0; $i < $totalFiles; $i++) {
-        $fileName = $_FILES['files']['name'][$i];
-        $targetFile = $targetDirectory . $fileName;
+        $fileName = $_FILES['images']['name'][$i];
+        $tmpName = $_FILES['images']['tmp_name'][$i];
+        $fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+        $targetFile = $targetDirectory . basename($fileName);
 
-        // Pindahkan file yang diunggah ke direktori penyimpanan
-        if(move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
-            echo "File $fileName berhasil diunggah.<br>";
+        // Validasi tipe file
+         if (in_array($fileType, $allowedExtensions)) {
+            if (move_uploaded_file($tmpName, $targetFile)) {
+                echo "File <b>$fileName</b> berhasil diunggah.<br>";
+            } else {
+                echo "Gagal mengunggah file <b>$fileName</b>.<br>";
+            }
         } else {
-            echo "Gagal mengunggah file $fileName.<br>";
+            echo "File <b>$fileName</b> tidak valid. Hanya file gambar yang diizinkan.<br>";
         }
     }
 } else {
-    echo "Tidak ada file yang diunggah";
+    echo "Tidak ada file yang diunggah.";
 }
+?>
