247ee4cf3f35f1bdaaef9c09d1f27687215541bf
Triskalimatu20 <triskalimatu@gmail.com>
2025-10-22 10:57:35 +0700

    4_Menghias Upload File

--- upload.js
+++ upload.js
@@ -1,14 +1,22 @@
 $(document).ready(function () {
-  $("#upload-form").submit(function (e) {
-    e.preventDefault(); // Mencegah reload halaman
+  $("#file").change(function () {
+    if (this.files.length > 0) {
+      $("#upload-button").prop("disabled", false).css("opacity", 1);
+    } else {
+      $("#upload-button").prop("disabled", true).css("opacity", 0.5);
+    }
+  });
 
-    var formData = new FormData(this); // Ambil data dari form
+  $("#upload-form").submit(function (e) {
+    e.preventDefault();
+
+    var formData = new FormData(this);
 
     $.ajax({
       type: "POST",
-      url: "upload_ajax.php", // File PHP tujuan
+      url: "upload_ajax.php",
       data: formData,
-      cache: false, // HARUS tanpa tanda kutip
+      cache: false,
       contentType: false,
       processData: false,
       success: function (response) {
